a38d0197a2f5fa4c5dc69a455cf687bcae93f5a3
Metehan Ozten <m_ozten@yahoo.com>
2016-05-04 15:33:03 -0700

    Update redo.py

--- redo.py
+++ redo.py
@@ -1,43 +1,10 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Evaluation for CIFAR-10.
-
-Accuracy:
-cifar10_train.py achieves 83.0% accuracy after 100K steps (256 epochs
-of data) as judged by cifar10_eval.py.
-
-Speed:
-On a single Tesla K40, cifar10_train.py processes a single batch of 128 images
-in 0.25-0.35 sec (i.e. 350 - 600 images /sec). The model reaches ~86%
-accuracy after 100K steps in 8 hours of training time.
-
-Usage:
-Please see the tutorial and website for how to download the CIFAR-10
-data set, compile the program and train the model.
-
-http://tensorflow.org/tutorials/deep_cnn/
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from datetime import datetime
-import math
-import time
-import os
+import math, time, os, sys
+
 import numpy as np
 import tensorflow as tf
 import conv_net
@@ -135,7 +102,9 @@ def choose_model():
       print('No checkpoint file found exiting')
       return None, None
 
-def main(argv=None):  # pylint: disable=unused-argument
+def main(argv=sys.argv):
+  if len(sys.argv > 1):
+    FLAGS.data_dir = sys.argv[1]
   conv_net.maybe_download_and_extract()
   if tf.gfile.Exists(FLAGS.eval_dir):
     tf.gfile.DeleteRecursively(FLAGS.eval_dir)

1f4ff7a9b0af24eb72a9c28e932c7a4745f640e8
Metehan Ozten <m_ozten@yahoo.com>
2016-05-04 17:28:02 -0700

    Changing number of iterations

--- conv_net.py
+++ conv_net.py
@@ -26,7 +26,7 @@ tf.app.flags.DEFINE_string('data_dir', os.path.join(os.getcwd(),'cifar100_data')
 tf.app.flags.DEFINE_string('train_dir', os.path.join(os.getcwd(),'cifar100_train'),
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 20000,
+tf.app.flags.DEFINE_integer('max_steps', 2000,
                             """Number of batches to run.""")
 
                             

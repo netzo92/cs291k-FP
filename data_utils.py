9d8039852764d86fff0703130b32995cad5a50d0
Metehan Ozten <m_ozten@yahoo.com>
2016-05-24 18:10:52 -0700

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -24,7 +24,7 @@ import random
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
-
+FLAGS = tf.app.flags.FLAGS
 # Process images of this size. Note that this differs from the original CIFAR
 # image size of 32 x 32. If one alters this number, then the entire model
 # architecture will change and any model would need to be retrained.
